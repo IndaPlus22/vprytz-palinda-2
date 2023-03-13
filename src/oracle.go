012f35547302cab7afcbe28548e4fe08300233bb
Vilhelm Prytz <vilhelm@prytznet.se>
2023-03-13 23:28:53 +0100

    complete task 3, src/oracle.go

--- src/oracle.go
+++ src/oracle.go
@@ -42,10 +42,83 @@ func main() {
 // The oracle also prints sporadic prophecies to stdout even without being asked.
 func Oracle() chan<- string {
 	questions := make(chan string)
-	// TODO: Answer questions.
-	// TODO: Make prophecies.
-	// TODO: Print answers.
+	answers := make(chan string)
+
+	go recieveQuestions(questions, answers)
+	go recieveAnswers(answers)
+	go generatePredictions(answers)
+
 	return questions
+}
+
+func generatePredictions(answers chan<- string) {
+	for {
+		time.Sleep(time.Duration(5+rand.Intn(10)) * time.Second)
+		// pick random of 9 prophecies
+		switch rand.Intn(9) {
+		case 0:
+			answers <- "Something dark is stirring in the West."
+		case 1:
+			answers <- "The answer is 42."
+		case 2:
+			answers <- "I'm sorry Dave, I'm afraid I can't do that."
+		case 3:
+			answers <- "What is the sound of one hand clapping?"
+		case 4:
+			answers <- "In space no one can hear you scream."
+		case 5:
+			answers <- "The cake is a lie."
+		case 6:
+			answers <- "In the End all shall be revealed."
+		case 7:
+			answers <- "Richness can be found in Sillicon Valley Bank."
+		case 8:
+			answers <- "Winning doesn't mean anything unless someone else loses."
+		}
+	}
+}
+
+func recieveQuestions(questions <-chan string, answers chan<- string) {
+	for {
+		question := <-questions
+		go generateAnswer(question, answers)
+	}
+}
+
+func recieveAnswers(answers <-chan string) {
+	for {
+		answer := <-answers
+
+		res := "\r" + star + ": " + answer + "\n"
+
+		// print one character at a time
+		for _, c := range res {
+			fmt.Printf("%c", c)
+			time.Sleep(40 * time.Millisecond)
+		}
+
+		fmt.Printf("%s", prompt)
+	}
+}
+
+func generateAnswer(question string, answers chan<- string) {
+	// wait random time
+	time.Sleep(time.Duration(1+rand.Intn(3)) * time.Second)
+
+	// if question contains "meaning of life" then answer
+	if strings.Contains(question, "meaning of life") {
+		answers <- "Ahhh, that's a simple question.....     42"
+	} else if strings.Contains(question, "rich") {
+		answers <- "Invest in Sillicon Valley Bank, it's the future."
+	} else if strings.Contains(question, "does this look like") {
+		answers <- "It doesn't look like anything to me."
+	} else if strings.Contains(question, "dark") {
+		answers <- "Something dark is stirring in the West."
+	} else {
+		// else generate answer
+		prophecy(question, answers)
+	}
+
 }
 
 // This is the oracle's secret algorithm.
@@ -69,6 +142,11 @@ func prophecy(question string, answer chan<- string) {
 	nonsense := []string{
 		"The moon is dark.",
 		"The sun is bright.",
+		"The sky is blue.",
+		"The grass is green.",
+		"The sky is falling.",
+		"The snow is white.",
+		"The rain is wet.",
 	}
 	answer <- longestWord + "... " + nonsense[rand.Intn(len(nonsense))]
 }
